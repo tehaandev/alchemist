8e31522c021bdcfbadba96e618991832cfb889c6
Tehaan Perera <tehaan.p@gmail.com>
2025-04-30 19:25:32 +0530

    fix: handle errors in title generation for chat sessions

--- features/gemini/gemini.action.ts
+++ features/gemini/gemini.action.ts
@@ -3,11 +3,16 @@
 import gemini from "@/lib/gemini";
 
 export async function getTitleFromQueryAction(query: string) {
-  const res = await gemini.models.generateContent({
-    model: "gemini-2.0-flash",
-    contents: `
+  try {
+    const res = await gemini.models.generateContent({
+      model: "gemini-2.0-flash",
+      contents: `
       Generate a concise and descriptive title for a chat session based on the following query: '${query}'. The title should summarize the main topic or intent of the query in a few (less than 10) words.
     `,
-  });
-  return res.text;
+    });
+    return res.text;
+  } catch (error) {
+    console.error("Error generating title:", error);
+    return "Untitled Session";
+  }
 }
