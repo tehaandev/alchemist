c490c6e6a121a689b37491a4f095124cdb607983
Tehaan Perera <tehaan.p@gmail.com>
2025-04-30 23:14:04 +0530

    fix: update model in getTitleFromQueryAction to use the correct version for title generation

--- features/gemini/gemini.action.ts
+++ features/gemini/gemini.action.ts
@@ -5,12 +5,12 @@ import gemini from "@/lib/gemini";
 export async function getTitleFromQueryAction(query: string) {
   try {
     const res = await gemini.models.generateContent({
-      model: "gemini-2.0-flash",
+      model: "gemini-2.0-flash-lite",
       contents: `
       Generate a concise and descriptive title for a chat session based on the following query: '${query}'. The title should summarize the main topic or intent of the query in a few (less than 10) words.
     `,
     });
-    return res.text;
+    return res?.candidates?.[0]?.content?.parts?.[0]?.text;
   } catch (error) {
     console.error("Error generating title:", error);
     return "Untitled Session";
